d32ab052e5f06326d4ef63bb2caabdaf990cf1c0
russab0 <r.sabirov@innopolis.ru>
2020-05-05 12:00:10 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Russian-Tatar and Tatar-Russian traslator. Project for NLP&ML course at Innopolis University.',
+    author='Kamil Akhmetov, Ruslan Sabirov, Innopolis University',
+    license='',
+)
