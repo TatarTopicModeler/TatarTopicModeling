3776569d4b6e80558430bf63874d790c7e66c732
Kamil Akhmetov <kamilkoduo@gmail.com>
2020-05-06 17:57:54 +0300

    + fixes

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -7,15 +7,18 @@ import nltk
 import os
 import re
 
-PATH = Path(os.getcwd())  # Project path
-tt_stopwords_file = open(PATH / 'data/external/stopwords_tt.txt', 'r')  # Stopwords file
-ru_stopwords_file = open(PATH / 'data/external/stopwords_ru.txt', 'r')  # Stopwords file (all nltk words are included)
-dataset_stopwords_file = open(PATH / 'data/interim/stopwords_dataset.txt', 'r')  # Dataset specific stopwords
+PATH_STOPWORDS_TT = Path('data/external/stopwords_tt.csv') # Stopwords file
+PATH_STOPWORDS_RU = Path('data/external/stopwords_ru.csv') # Stopwords file (all nltk words are included)
+PATH_STOPWORDS_INTERIM = Path('data/interim/stopwords_dataset.csv')
 
-tt_stopwords = [line.strip() for line in tt_stopwords_file]
-ru_stopwords = [line.strip() for line in ru_stopwords_file]
-dataset_stopwords = [line.strip() for line in dataset_stopwords_file]
-STOPWORDS = tt_stopwords + ru_stopwords + dataset_stopwords
+dataset_stopwords_file = open(PATH_STOPWORDS_INTERIM, 'r')  # Dataset specific stopwords
+
+tt_stopwords = pd.read_csv(PATH_STOPWORDS_TT, header=None)
+ru_stopwords = pd.read_csv(PATH_STOPWORDS_RU, header=None)
+# interim_stopwords = pd.read_csv(PATH_STOPWORDS_INTERIM, header=None)
+
+
+STOPWORDS = pd.DataFrame().append([tt_stopwords, ru_stopwords])
 
 URL_EXP = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'
 russian_stemmer = SnowballStemmer('russian')
@@ -28,21 +31,24 @@ ALPHA = ENGLISH_ALPHA.union(RUSSIAN_ALPHA).union(TATAR_ALPHA)  # union of three 
 PUNCT = r'''!"#$%&\'()*+,-./:;<=>?@\\][^_`{|}~«»—“”•'''  # punctuation marks
 
 
-def remove_accents(text):
+def remove_stress(text):
     nfkd_form = unicodedata.normalize('NFKD', text)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 
 def normalize(text):
     text = text.lower()
-    text = re.sub(URL_EXP, ' ', text)  # remove URLS
+    # text = re.sub(URL_EXP, ' ', text)  # remove URLS
+    print(text)
     text = re.sub(f'[{PUNCT}]', ' ', text)  # remove all punctuation signs
+    print(text)
     text = re.sub('[0-9]', ' ', text)  # replace all digits with spaces
-    text = remove_accents(text)
-    result = " ".join([x.lower() for x in text.split()])  # lower all letters and delete all doubled spaces
-    # TODO delete stress marks
-    return result
-
+    print(text)
+    text = re.sub(r'\s+', ' ', text)
+    print(text)
+    text = remove_stress(text)
+    print()
+    return text
 
 def is_english(word):
     return sum([int(char in ENGLISH_ALPHA) for char in word]) >= len(word) // 2
@@ -83,7 +89,10 @@ if __name__ == '__main__':
     t1 = """Савытка йомырка сыталар, сөт салалар, май, шикәр комы, тоз, азрак чүпрә өстиләр (күп вакытта чүпрә салмыйлар да). Шул катнашманы шикәре, тозы һәм чүпрәсе эреп беткәнче яхшылап болгаталар. Аннары он кушып, токмач камырыннан йомшаграк итеп камыр басалар. Камырны 100—150 г лы кисәкләргә бүләләр дә бармак калынлыгында баусыман тәгәрәтәләр һәм урман чикләвеге кадәр итеп турыйлар. Кайнап торган майга салалар, болгата-болгата, кызарганчы пешерәләр.
     Әзер бавырсакны тишекле зур чүмечтә сөзәләр, мае агып беткәч, өстенә шикәр оны сибергә була. Бавырсакны чәй янына чыгаралар. Аны катык һәм сөт белән дә ашарга була. Ул юлга алу өчен дә әйбәт.
     1 кг югары сортлы онга: 10 йомырка, 130—140 г сөт, 30—35 г шикәр комы, 30 г сары май, 5 г чүпрә, тол, пешерү өчен 180—200 г май."""
-    t2 = pd.read_csv(PATH / 'data/raw/contents.csv', sep=',')['content'].iloc[0]
-    prep = preprocess(t2, stem=True, lemm=False)
-    print(prep)
-    print(remove_accents('кәбестә́'))
+    # t2 = pd.read_csv(PATH / 'data/raw/contents.csv', sep=',')['content'].iloc[0]
+    # prep = preprocess(t1, stem=True, lemm=False)
+    # print(prep)
+    # print(remove_stress('кәбестә́'))
+    test = normalize(t1)
+    print(t1)
+    print(test)
