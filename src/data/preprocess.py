2e84c0831d454897502c5edb7ace7192a47152b3
Kamil Akhmetov <kamilkoduo@gmail.com>
2020-05-06 22:24:14 +0300

    + work

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -7,8 +7,8 @@ import nltk
 import os
 import re
 
-PATH_STOPWORDS_TT = Path('data/external/stopwords_tt.csv') # Stopwords file
-PATH_STOPWORDS_RU = Path('data/external/stopwords_ru.csv') # Stopwords file (all nltk words are included)
+PATH_STOPWORDS_TT = Path('data/external/stopwords_tt.csv')  # Stopwords file
+PATH_STOPWORDS_RU = Path('data/external/stopwords_ru.csv')  # Stopwords file (all nltk words are included)
 PATH_STOPWORDS_INTERIM = Path('data/interim/stopwords_dataset.csv')
 
 dataset_stopwords_file = open(PATH_STOPWORDS_INTERIM, 'r')  # Dataset specific stopwords
@@ -24,34 +24,33 @@ URL_EXP = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^
 russian_stemmer = SnowballStemmer('russian')
 english_stemmer = SnowballStemmer('english')
 
-ENGLISH_ALPHA = {chr(x) for x in range(ord('a'), ord('z') + 1)}
-RUSSIAN_ALPHA = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
-TATAR_ALPHA = set('аәбвгдеёжҗзийклмнңоөпрстуүфхһцчшщъыьэюя')
-ALPHA = ENGLISH_ALPHA.union(RUSSIAN_ALPHA).union(TATAR_ALPHA)  # union of three alphabets
+EN_ALPAH_REGEX = r'a-z'
+# RUSSIAN_ALPHA = r'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+TT_ALPHA_REGEX = r'аәбвгдеёжҗзийклмнңоөпрстуүфхһцчшщъыьэюя'
+ALPHA_REGEX = EN_ALPAH_REGEX + TT_ALPHA_REGEX
+
 PUNCT = r'''!"#$%&\'()*+,-./:;<=>?@\\][^_`{|}~«»—“”•'''  # punctuation marks
 
 
-def remove_stress(text):
-    nfkd_form = unicodedata.normalize('NFKD', text)
-    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+def remove_stress(text, ignore='йё'):
+    normalized = unicodedata.normalize('NFKC', text)
+    normalized = [n for n in normalized if not unicodedata.combining(n)]
+    return u''.join(normalized)
 
 
 def normalize(text):
     text = text.lower()
-    # text = re.sub(URL_EXP, ' ', text)  # remove URLS
     print(text)
-    text = re.sub(f'[{PUNCT}]', ' ', text)  # remove all punctuation signs
+    text = re.sub(URL_EXP, ' ', text)  # remove URLS
+    text = remove_stress(text)  # normalize all chars expect ['й','ё']
     print(text)
-    text = re.sub('[0-9]', ' ', text)  # replace all digits with spaces
-    print(text)
-    text = re.sub(r'\s+', ' ', text)
-    print(text)
-    text = remove_stress(text)
-    print()
+    text = re.sub(f'[^{ALPHA_REGEX}]', ' ', text)  # leave only letters
+    text = re.sub(r'\s+', ' ', text)  # remove extra spaces
     return text
 
+
 def is_english(word):
-    return sum([int(char in ENGLISH_ALPHA) for char in word]) >= len(word) // 2
+    return sum([int(char in EN_ALPAH_REGEX) for char in word]) >= len(word) // 2
 
 
 def stemming(words):  # TODO adapt for tatar (implement a new one or find edit distance with dictionary word
@@ -59,6 +58,10 @@ def stemming(words):  # TODO adapt for tatar (implement a new one or find edit d
         english_stemmer.stem(word) if is_english(word) else russian_stemmer.stem(word)
         for word in words
     ]
+
+
+def stem(word):
+    pass
 
 
 def lemmatize(text):  # TODO не работает
@@ -86,7 +89,7 @@ def preprocess(documents, stem=False, lemm=False):
 
 
 if __name__ == '__main__':
-    t1 = """Савытка йомырка сыталар, сөт салалар, май, шикәр комы, тоз, азрак чүпрә өстиләр (күп вакытта чүпрә салмыйлар да). Шул катнашманы шикәре, тозы һәм чүпрәсе эреп беткәнче яхшылап болгаталар. Аннары он кушып, токмач камырыннан йомшаграк итеп камыр басалар. Камырны 100—150 г лы кисәкләргә бүләләр дә бармак калынлыгында баусыман тәгәрәтәләр һәм урман чикләвеге кадәр итеп турыйлар. Кайнап торган майга салалар, болгата-болгата, кызарганчы пешерәләр.
+    t1 = """бавырса́к Савытка йомырка сыталар, сөт салалар, май, шикәр комы, тоз, азрак чүпрә өстиләр (күп вакытта чүпрә салмыйлар да). Шул катнашманы шикәре, тозы һәм чүпрәсе эреп беткәнче яхшылап болгаталар. Аннары он кушып, токмач камырыннан йомшаграк итеп камыр басалар. Камырны 100—150 г лы кисәкләргә бүләләр дә бармак калынлыгында баусыман тәгәрәтәләр һәм урман чикләвеге кадәр итеп турыйлар. Кайнап торган майга салалар, болгата-болгата, кызарганчы пешерәләр.
     Әзер бавырсакны тишекле зур чүмечтә сөзәләр, мае агып беткәч, өстенә шикәр оны сибергә була. Бавырсакны чәй янына чыгаралар. Аны катык һәм сөт белән дә ашарга була. Ул юлга алу өчен дә әйбәт.
     1 кг югары сортлы онга: 10 йомырка, 130—140 г сөт, 30—35 г шикәр комы, 30 г сары май, 5 г чүпрә, тол, пешерү өчен 180—200 г май."""
     # t2 = pd.read_csv(PATH / 'data/raw/contents.csv', sep=',')['content'].iloc[0]
@@ -94,5 +97,5 @@ if __name__ == '__main__':
     # print(prep)
     # print(remove_stress('кәбестә́'))
     test = normalize(t1)
-    print(t1)
-    print(test)
+    # print(t1)
+    # print(test)
